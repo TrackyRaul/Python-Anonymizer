35a619500ab5b7eaf2c5c0a53e5c99dcaad56a4a
pietromoretto <38654875+pietromoretto@users.noreply.github.com>
2019-01-28 11:45:33 +0100

    Added Mail.py

--- Mail.py
+++ Mail.py
@@ -1,3 +1,17 @@
 class Mail():
-    def __init__():
-        pass
+
+    def __init__(self, name, surname, mail):
+        self.name = name
+        self.surname = surname
+        self.domain = self.__get_domain_from_mail(mail)
+        self.mail = self.__generate_mail()
+
+    def __get_domain_from_mail(self, mail):
+        return mail[mail.rfind('@') + 1 : ]
+
+    def __generate_mail(self):
+        #self.new_mail += f"{self.name}.{self.surname}@{self.domain}"
+        return "{}.{}@{}".format(self.name, self.surname, self.domain)
+    
+    def get_mail(self):
+        return self.mail
