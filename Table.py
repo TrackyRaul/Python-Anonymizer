4d6f647d1ab8f4f7c9c07ef0f6998cd69f2873c3
Raul <raul.farkas19@gmail.com>
2019-03-11 09:15:28 +0100

    Minor fixes

--- Table.py
+++ Table.py
@@ -1,5 +1,7 @@
 import configurator
 from modules import Mail, Date, FiscalCode, String
+import re
+
 conf = configurator.configure("config.json")
 
 
@@ -8,17 +10,24 @@ class Table():
         """Attributes"""
         self.entries = []
         self.header = []
+        self.column_types = {}
         self.file_info = conf.file
         self.glb_info = conf.gbl
+
         """Load data from file"""
         self.header = self.__load_file()[0]
         self.rows = self.__load_file()[1]
 
         """Filter header and rows based on header specified in configuration file"""
-        self.header = self.__filter_info(self.header, self.rows)[0]
-        self.rows = self.__filter_info(self.header, self.rows)[1]
+        filtered_info = self.__filter_info(self.header, self.rows)
+        self.header = filtered_info[0]
+        self.rows = filtered_info[1]
 
-        print(self.header,self.rows)
+        """Set header column types"""
+        self.column_types = self.__get_column_types(self.header,self.rows)
+
+
+
 
     def __load_file(self):
         """Load data from files"""
@@ -59,3 +68,52 @@ class Table():
             new_rows.append(new_row)
 
         return(new_header, new_rows)
+
+    def __get_column_types(self,header,rows):
+        """Read from configuration the types or calculate them based on regex"""
+        types = {}
+        for i in range(len(header)):
+            typed = False
+            if self.__exist_column_conf(header[i]):
+                #Check if field has type attribute
+                if hasattr(getattr(conf.fields,header[i]),type):
+                    #Create a class reference based on stated type in configuration file
+                    types[header[i]] = globals()[getattr(conf.fields,header[i]).type]
+                    typed = True
+
+            if not typed:
+                #Give type based on regex in conf file
+                types[header[i]] = self.__get_type_from_regex(header[i],rows[0][i])
+                
+        #Check if the difference between the keys of types dict and header is an empty list
+        if len([x for x in list(types.keys()) if x in header]) != 0:
+            #If the difference is not empty some columns don't have a type
+            raise Exception("Not all columns have type declared properly!")
+
+        return types
+
+
+    def __exist_column_conf(self,column):
+        """Check if field has costum configuration"""
+        return_value = False
+        if hasattr(conf.fields,column):
+            return_value = True
+
+    def __get_type_from_regex(self,column,sample_row_value):
+        """Given a string get the type of the string based on regex"""
+        return_type = None
+        for type in conf.gbl.data_types:
+            #Get patter from configuration file
+            pattern = re.compile(getattr(conf.gbl,type).structure)
+            if pattern.match(sample_row_value):
+                #Check for all available types if one matches
+                return_type = globals()[type]
+                break
+        if return_type == None:
+            #Check if the type was not given
+            raise Exception(f"Type could not be found for {column}")
+        return return_type
+
+            
+
+
