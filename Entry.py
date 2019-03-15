59e0418bad638501432179512d090e0350cb6069
Raul <raul.farkas19@gmail.com>
2019-03-15 10:43:01 +0100

    Fixed functionality on requirements passed when creating objects. Started working on the fiscalcode class.

--- Entry.py
+++ Entry.py
@@ -10,7 +10,7 @@ class Entry():
         """Consider each column and anonymize it"""
         for attr in conf.fields_list:
             requirements = self.__parse_req(attr)
-
+            
             #Anonymize each attribute
             getattr(self,attr).anonymize(requirements)
 
@@ -18,20 +18,21 @@ class Entry():
     def __parse_req(self,attr):
         """Get from configuration file the required variables and values"""
         req = {}
-        if hasattr(getattr(conf.fields,attr),attr):
-            req_ref = getattr(conf.fields,attr).req
+        if hasattr(getattr(conf.fields,attr),"req"):
             
+            req_ref = getattr(conf.fields,attr).req
+
             for i in range(len(req_ref)):
                 #Check if the variable referenced is the original content or not
                 pattern = re.compile("^\$ORIGINAL.[a-zA-Z]*")
 
                 if(pattern.match(req_ref[i])):
+ 
                     #Set the value with the original value of the refferenced variable
-                    req[req_ref[i]] = getattr(self,req_ref[i].replace("$","").original_value)
+                    req[req_ref[i]] = getattr(self,req_ref[i].replace("$","").split(".")[1]).original_value
                 else:
                     #Set the value with the new value of the refferenced variable
-                    req[req_ref[i]] = getattr(self,req_ref[i].replace("$","").value)
-        
+                    req[req_ref[i]] = getattr(self,req_ref[i].replace("$","")).value
         return req
 
     def print(self,separator = ","):
